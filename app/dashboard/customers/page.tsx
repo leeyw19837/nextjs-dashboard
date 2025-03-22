1cc57155eaa636824b58af1ec1ff6aa913ff55df
leeyw19837 <leeyw19837@hotmail.com>
2025-03-22 18:42:53 +0800

    init commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomersPage() {
+    return (
+        <div>
+            <h1>Customers</h1>
+            <p>Manage your customers here!</p>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function () {
+    return (
+        <div>
+            <h1>Invoices</h1>
+            <p>Manage your invoices here!</p>
+        </div>
+    )
+}
